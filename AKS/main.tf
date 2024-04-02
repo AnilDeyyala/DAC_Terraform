e40c88d2323ec082e458cc761bae859a0e4fa6e4
AnilDeyyala <anil.deyyala@tigeranalytics.local>
2024-04-02 15:30:07 +0530

    updated AKS tf code

--- AKS/main.tf
+++ AKS/main.tf
@@ -4,6 +4,7 @@ resource "azurerm_resource_group" "resource_group" {
 }
 
 resource "azurerm_kubernetes_cluster" "cluster" {
+  depends_on = [ azurerm_resource_group.resource_group ]
   name                      = var.clustername
   location                  = azurerm_resource_group.resource_group.location
   resource_group_name       = azurerm_resource_group.resource_group.name
